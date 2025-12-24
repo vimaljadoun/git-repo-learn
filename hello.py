879273727e9dc98aa3b2e945a45ff424ac5f8a15
vimaljadoun <vimalkumar96390@gmail.com>
2025-12-24 20:05:22 +0530

    added new lines about matplotlib code

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def factorial(n):
     if n == 0:
         return 1
@@ -7,3 +11,16 @@ def factorial(n):
 number = 5
 result = factorial(number)
 print(f"The factorial of {number} is {result}")
+
+x = np.arange(0,10,0.1)
+
+y = np.sin(x)
+
+plt.plot(x,y,'-', label='sin(x)')
+plt.plot(0,0)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.tight_layout()
+plt.savefig('sinx.png',dpi=300)
+plt.show()

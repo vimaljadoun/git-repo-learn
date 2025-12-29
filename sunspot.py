59f715a618f836d826ee3bd90bb9947e59914fe5
vimaljadoun <vimalkumar96390@gmail.com>
2025-12-29 18:31:25 +0530

    modified files

--- sunspot.py
+++ sunspot.py
@@ -8,24 +8,17 @@ font = {'family': 'serif',
         }
 plt.rcParams.update(plt.rcParamsDefault)
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n*factorial(n-1)
-number = 5
-result = factorial(number)
-print(f"The factorial of {number} is {result}")
 
-x = np.arange(-10,10,0.1)
-y1 = np.sin(x)
-y2 = np.cos(x)
+import matplotlib.pyplot as plt
 
-plt.plot(x,y1,'-', label='sin(x)')
-plt.plot(x,y2,'-', label='cos(x)')
-plt.plot(0,0)
-plt.xlabel('x')
-plt.ylabel('y')
+# Read two-column data
+year, sunspots = np.loadtxt("sunspotdata.txt",unpack=True,comments="#")
+
+#plot
+plt.plot(year,sunspots,'-', label='Sun')
+plt.xlabel('Year')
+plt.ylabel('Sunspot Number')
+plt.title('Relative Sunspot Numbers')
 plt.legend()
 plt.minorticks_on()
 plt.tick_params(axis="x", which="minor", direction="in", top=True)
@@ -33,5 +26,5 @@ plt.tick_params(axis="x", which="major", direction="in", top=True, labeltop=Fals
 plt.tick_params(axis="y", which="minor", direction="in", right=True)
 plt.tick_params(axis="y", which="major", direction="in", right=True, labelright=False, left=True, labelleft=True)
 plt.tight_layout()
-plt.savefig('trignometric-func.png',dpi=300)
+plt.savefig('sunspot.png',dpi=300)
 plt.show()
